5ab0224379c4b6a397d0ad96d5783665684fdfa7
Kamidake <x9k92007@gmail.com>
2025-07-17 13:47:01 +0800

    Update playlist-transformer.js

--- playlist-transformer.js
+++ playlist-transformer.js
@@ -103,7 +103,7 @@ class PlaylistTransformer {
         }
 
         if (!headers['User-Agent']) {
-            headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36';
+            headers['User-Agent'] = 'exoplayer';
         }
         if (!headers['Referer']) {
             headers['Referer'] = 'https://streamtape.com/';
